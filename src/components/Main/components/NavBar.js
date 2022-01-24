9a643363d2617ec98c6d4f9400f2deabf10b5671
vivekpatel17 <vivekpatelk0@gamil.com>
2022-01-25 01:40:08 +0530

    menu changes

--- src/components/Main/components/NavBar.js
+++ src/components/Main/components/NavBar.js
@@ -3,16 +3,11 @@ import classes from  "./NavBar.module.css";
 
 import logo from "../images/Logo.svg";
 import dropdownIcon from "../images/dropdownicon.svg";
+import HamburgerIcon from "../images/hamburger-menu.svg";
 
 function NavBar() {
 
     const nav = ["Destinations", "Hotels", "Flights", "Bookings", "Login"];
-
-    function handle_dropdown_click(){
-        return(
-            nav.map((data) => <p>{data}</p>)
-        );
-    }
 
     return (
         <div className={classes.navbar}>
@@ -20,14 +15,14 @@ function NavBar() {
             <div className={classes.menu}>
                 <div className={classes.menuitem}>
                     {nav.map((data) => <p>{data}</p>)}
+                    <button>
+                        <img src={HamburgerIcon} alt="" />
+                    </button>
                 </div>
                 <div className={classes.signup}>
                     <button>Sign up</button>
                 </div>
-                <div 
-                    className={classes.menudrop}
-                    onClick={handle_dropdown_click}
-                >
+                <div className={classes.menudrop}>
                     <p>EN<img src={dropdownIcon} alt="" /></p>
                 </div>
             </div>
